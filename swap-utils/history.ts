5ec9e926d40a7fe7f807f9613758eb8295821448
rkmonarch <rahulkulkarni5234@gmail.com>
2024-06-23 23:23:44 +0800

    feat: changed api-key

--- swap-utils/history.ts
+++ swap-utils/history.ts
@@ -1,7 +1,13 @@
 
 async function walletHistory(address: string) {
     const url = `https://voyager.online/api/txns?to${address}=&ps=10&p=1&type=null`
-    const response = await fetch(url);
+    const response = await fetch(url, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'x-api-key': process.env.VOYAGER_API_KEY as string
+        },
+    });
     const data = await response.json();
     return data;
 }
